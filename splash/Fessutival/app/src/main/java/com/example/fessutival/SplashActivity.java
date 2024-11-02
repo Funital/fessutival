70352d6202287b76e436bbe107d430972af8509c
Funital <gksrmawns0925@naver.com>
2024-11-02 11:24:36 +0900

    [fessutival/splash] splash 화면 생성

---
+++ splash/Fessutival/app/src/main/java/com/example/fessutival/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.fessutival;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // splash 3초동안
+        Handler handler = new Handler();
+        handler.postDelayed(new splashHandler(), 3000);
+    }
+
+    private class splashHandler implements Runnable {
+        @Override
+        public void run() {
+            startActivity(new Intent(getApplication(), MainActivity.class));
+            SplashActivity.this.finish();
+        }
+    }
+}
